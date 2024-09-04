82dc39babb5af161b4d317c52b1d5fe03fc3f824
Tae90 <xogus54@gmail.com>
2024-09-04 23:35:37 +0900

    [feat] 이벤트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-function Header(props) {
-  console.log('props', props.title)
-  return <header>
-    <h1><a href="/">{props.title}</a></h1>
-  </header>
-}
-function Nav(props) {
-  const lis = []
-  for (let i = 0; i < props.topics.length; i++) {
-    let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li >)
-  }
-  return <nav>
-    <ol>
-      {lis}
-    </ol>
-  </nav>
-}
+
+
 function Article(props) {
   return <article>
     <h2>Welcome</h2>
@@ -26,6 +10,32 @@ function Article(props) {
     {props.body} */}
   </article>
 }
+function Header(props) {
+  console.log('props', props.title)
+  return <header>
+    <h1><a href="/" onClick={(event) => {
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
+  </header>
+}
+function Nav(props) {
+  const lis = []
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/' + t.id} onClick={(event) => {
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a>
+    </li >)
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
+}
 function App() {
   const topics = [
     { id: 1, title: 'html', body: 'html is ...' },
@@ -33,9 +43,13 @@ function App() {
     { id: 3, title: 'javascript', body: 'javascript ...' }
   ]
   return (
-    <div className="App">
-      <Header title="React"></Header>
-      <Nav topics={topics}></Nav>
+    <div>
+      <Header title="web" onChangeMode={function () {
+        alert('Header')
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id) => {
+        alert(id);
+      }}></Nav>
       <Article title="Hi" body="Hello, WEB"></Article>
     </div>
   );
